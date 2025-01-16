e9b7dc17841ab1380b1070e41d36ea84e2dad180
Nithinkumar U <105657382+Nithinkumar16@users.noreply.github.com>
2025-01-16 18:53:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Basic React App</h1>
+        <p>This is a simple React application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
